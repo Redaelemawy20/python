2d4b5ff2a53648eb29a4c549641dc7396927278a
redaelemawy <redaelemawy98@gmail.com>
2026-04-12 18:46:50 +0200

    module

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-current_year = 2026
+import utils;
 
-def get_info():
-    name = input('your name:')
-    age = input('age:')
-    birth_year =0
-    try:
-        birth_year = current_year - int(age)
-    except:
-        print('enter valid age')
-    print_info(year= birth_year, name=name)
+name, year = utils.get_info()
 
-def print_info(name, year):
-    print("Hi {} your birth year is {}".format(name, year))
-
-get_info()
+utils.print_info(name, year)
---
+++ utils.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def get_info():
+    name = input('your name: ')
+    age = input('age: ')
+    
+    try:
+        current_year = datetime.now().year
+        birth_year = current_year - int(age)
+        return name, birth_year
+    except:
+        print('enter valid age')
+        return None, None
+
+
+def print_info(name, year):
+    print("Hi {} your birth year is {}".format(name, year))
