2914c6c21aef1a27d234484ac42c37e3f3d38ba1
redaelemawy <redaelemawy98@gmail.com>
2026-04-16 15:20:56 +0200

    add tuples

---
+++ tuples.py
@@ -0,0 +1,23 @@
+t = (1, 2, 3)
+
+print(t)
+print(type(t))
+
+
+m = (3, 4, 'TT')
+
+print(len(m))
+
+m = m + ('N',)
+print(m)
+# tu = tuple(1) ## error
+
+# print(tu) 
+
+
+print((0, 20, 4, 5, 520) > (0, 20, 4, 5, 100, 1199))
+
+dct = {('Jhon', '34', 'Manager'): 4000}
+print(dct)
+print(dct.keys())
+
